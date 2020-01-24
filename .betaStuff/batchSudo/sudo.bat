afe9f4752258edc8003febaad75fac9720b94ed9
Mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-01-23 20:57:40 -0500

    Update sudo.bat

--- .betaStuff/batchSudo/sudo.bat
+++ .betaStuff/batchSudo/sudo.bat
@@ -5,6 +5,7 @@ goto sudo
 
 
 :sudo
+cls
 echo.
 echo Enter Command:
 set command=
@@ -15,10 +16,24 @@ goto commands
 :commands
 if %command%== $sudo_time.disp goto timedisp
 if %command%== $sudo_time.change goto changetime
-if %command%== $sudo_date.disp goto dispdate
-if %command%== $sudo_date.change goto changedate
-if %command%== $sudo_dtime.disp goto displaydatetime
-
+if %command%== $sudo_date.disp goto timedisp
+if %command%== $sudo_date.change goto changetime
+if %command%== $sudo_dtime.disp goto timedisp
+goto error
+:: batch comment  its same as else goto error
+:timedisp
+echo %time% %date
+echo When you are done viewing,
+pause
+goto sudo
+:changetime
+echo This command requires elevation. Go to batchOS-Open-Source/.betaStuff/batchSudo/sudo.bat and click run as admin
+set /p sudotime=change time (ex. 03:30:01):
+set /p sudodate=change date (ex. 06/02/01):
+time %sudotime%
+date %sudodate%
+cls
+goto sudo
 :error
 start error.vbs
 goto sudo
